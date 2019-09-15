41e6b650496f174375947075027be1a7bdfca22c
laoshanxi <178029200@qq.com>
2019-09-15 15:18:26 +0800

    change run_as to user

--- ApplicationManager/Application.cpp
+++ ApplicationManager/Application.cpp
@@ -276,7 +276,7 @@ web::json::value Application::AsJson(bool returnRuntimeInfo)
 
 	std::lock_guard<std::recursive_mutex> guard(m_mutex);
 	result[GET_STRING_T("name")] = web::json::value::string(GET_STRING_T(m_name));
-	if (m_user.length()) result[GET_STRING_T("run_as")] = web::json::value::string(GET_STRING_T(m_user));
+	if (m_user.length()) result[GET_STRING_T("user")] = web::json::value::string(GET_STRING_T(m_user));
 	result[GET_STRING_T("command_line")] = web::json::value::string(GET_STRING_T(m_commandLine));
 	if (m_workdir.length()) result[GET_STRING_T("working_dir")] = web::json::value::string(GET_STRING_T(m_workdir));
 	result[GET_STRING_T("status")] = web::json::value::number(m_status);
