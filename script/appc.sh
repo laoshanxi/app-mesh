fb255b06abdbfd72bb97837445679f7e54894a63
laoshanxi <178029200@qq.com>
2019-09-19 19:52:11 +0800

    export IFS='\n'

--- script/appc.sh
+++ script/appc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # by default, IFS is space which means space is string spliter
-export IFS=~
+export IFS='\n'
 export LD_LIBRARY_PATH=/opt/appmanager/lib64:$LD_LIBRARY_PATH
 /opt/appmanager/appc $@
