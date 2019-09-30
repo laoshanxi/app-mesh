e75b3a12ff0599464095b60916c2e216be41e361
laoshanxi <178029200@qq.com>
2019-09-30 11:43:49 +0800

    grep -v grep | grep -v appsvc.json

--- script/appmg_watchdog.sh
+++ script/appmg_watchdog.sh
@@ -12,10 +12,10 @@ cd /opt/appmanager/
 MYID="$$"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/appmanager/lib64
 while true ; do
-	case "$(ps aux | grep -w /opt/appmanager/appsvc | grep -v grep | wc -w)" in
+	case "$(ps aux | grep -w /opt/appmanager/appsvc | grep -v grep | grep -v appsvc.json | wc -w)" in
 	
 	0)	sleep 0.1
-		result=$(ps aux | grep -w /opt/appmanager/appsvc | grep -v grep | awk '{print $2}')
+		result=$(ps aux | grep -w /opt/appmanager/appsvc | grep -v grep | grep -v appsvc.json | awk '{print $2}')
 		if [ -z "$result" ]; then
 			nohup /opt/appmanager/appsvc >/dev/null 2>&1 &
 			log "Starting Application Manager:     $(date)"
@@ -29,7 +29,7 @@ while true ; do
 		sleep 2
 		;;
 	*)	# Only kill the process that was not started by this script
-		for i in $(ps aux | grep -w /opt/appmanager/appsvc | grep -v grep | awk '{print $2}')
+		for i in $(ps aux | grep -w /opt/appmanager/appsvc | grep -v grep | grep -v appsvc.json | awk '{print $2}')
 		  do
 			if [ $(pstree -Ap $MYID | grep $i | wc -w) -eq 0 ] ; then
 			  log "Killed duplicate Application Manager $i: $(date)"
