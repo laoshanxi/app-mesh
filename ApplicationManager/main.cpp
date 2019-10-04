b90a78cfe7161b2cbc710aae3761078d479ba69b
laoshanxi <178029200@qq.com>
2019-10-04 08:52:11 +0800

    dump

--- ApplicationManager/main.cpp
+++ ApplicationManager/main.cpp
@@ -27,6 +27,7 @@ int main(int argc, char * argv[])
 
 		// get configuration
 		auto config = Configuration::FromJson(Configuration::readConfiguration());
+		Configuration::instance()->dump();
 
 		// set log level
 		Utility::setLogLevel(config->getLogLevel());
