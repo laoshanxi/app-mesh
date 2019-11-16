a2ca3146cc7ceba2a6266ba0ee1c5ff991468771
laoshanxi <178029200@qq.com>
2019-11-16 15:40:56 +0800

    remane

--- script/appmanager.initd.sh
+++ script/appmanager.initd.sh
@@ -13,7 +13,7 @@
 
 ## Fill in name of program here.
 PROG="appsvc"
-PROG_WATCHDOG="appmgr-watchdog.sh"
+PROG_WATCHDOG="appmg-entrypoint.sh"
 PROGC="appc"
 PROG_PATH="/opt/appmanager" ## Not need, but sometimes helpful (if $PROG resides in /opt for example).
 PROG_ARGS="" 
