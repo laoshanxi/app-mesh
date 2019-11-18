c15af4eca26763c3aa3eee56fb4e1a24c604bf4f
laoshanxi <178029200@qq.com>
2019-11-18 09:24:15 +0800

    appc stop -> appc disable

--- script/pre_uninstall.sh
+++ script/pre_uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/appmanager/lib64
 if [ -f "/opt/appmanager/appc" ];then
-	/opt/appmanager/appc view -l | awk '{if (NR>1){cmd="/opt/appmanager/appc stop -n "$2;print(cmd);system(cmd)}}'
+	/opt/appmanager/appc view -l | awk '{if (NR>1){cmd="/opt/appmanager/appc disable -n "$2;print(cmd);system(cmd)}}'
 fi
