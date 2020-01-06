454521879967eb77617db16d2fb3c730f9f7b063
laoshanxi <178029200@qq.com>
2020-01-06 13:35:25 +0800

    Not stop app when uninstall

--- script/pre_uninstall.sh
+++ script/pre_uninstall.sh
@@ -5,6 +5,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/appmanager/lib64
 cp -f /opt/appmanager/appsvc.json /opt/appmanager/.appsvc.json
 
 # stop all running applications
-if [ -f "/opt/appmanager/appc" ];then
-	/opt/appmanager/appc view -l | awk '{if (NR>1){cmd="/opt/appmanager/appc disable -n "$2;print(cmd);system(cmd)}}'
-fi
+#if [ -f "/opt/appmanager/appc" ];then
+#	/opt/appmanager/appc view -l | awk '{if (NR>1){cmd="/opt/appmanager/appc disable -n "$2;print(cmd);system(cmd)}}'
+#fi
