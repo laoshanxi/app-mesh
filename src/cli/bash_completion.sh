5b5cf69bc5985373c6a3b328e911f034d6b2af4b
laoshanxi <178029200@qq.com>
2020-01-28 16:18:25 +0800

    Update bash_completion.sh

--- src/cli/bash_completion.sh
+++ src/cli/bash_completion.sh
@@ -1,37 +1,28 @@
-# appc completion                                     -*- shell-script -*-
+# appc(1) completion                                     -*- shell-script -*-
 
-if [[ $OSTYPE == *linux* ]]; then
-    . "$BASH_SOURCE.linux"
-    return
-fi
-
-# appc completion. This relies on the mount point being the third
-# space-delimited field in the output of appc
-#
 _appc_modeule()
 {
-        local cur prev OPTS
-        COMPREPLY=()
-        cur="${COMP_WORDS[COMP_CWORD]}"
-        prev="${COMP_WORDS[COMP_CWORD-1]}"
-        case $cur in
-                'appc')
-                        COMPREPLY=( $(compgen -W "logon logoff view resource label enable disable restart reg unreg run get put config passwd lock log" -- $cur) )
-                        return 0
-                        ;;
-        esac
-        case $cur in
-                '-n' | '--name')
-                        local APPS=`appc view | awk '{print $2}'`
-                        COMPREPLY=( $(compgen -W "$APPS" -- $cur) )
-                        return 0
-                        ;;
-                '-h'|'-v')
-                        return 0
-                        ;;
-        esac
-        return 0
-} && 
+    local cur prev words cword
+    _init_completion || return
+    
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    case $cur in
+        appc)
+            COMPREPLY=( $(compgen -W "logon logoff view resource label enable disable restart reg unreg run get put config passwd lock log" -- $cur) )
+            return 0
+            ;;
+        -n|--name)
+            local APPS=`appc view | awk '{print $2}'`
+            COMPREPLY=( $(compgen -W "$APPS" -- $cur) )
+            return 0
+            ;;
+        -h|-v)
+            return 0
+            ;;
+    esac
+    return 0
+} &&
 complete -F _appc_modeule appc
 
 # ex: ts=4 sw=4 et filetype=sh
