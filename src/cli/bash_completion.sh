ecb93e933ddb508b798c9f3a7d0d0b3d436cf346
laoshanxi <178029200@qq.com>
2020-01-28 16:19:27 +0800

    Update bash_completion.sh

--- src/cli/bash_completion.sh
+++ src/cli/bash_completion.sh
@@ -1,28 +1,25 @@
 # appc(1) completion                                     -*- shell-script -*-
 
-_appc_modeule()
+_appc()
 {
     local cur prev words cword
     _init_completion || return
-    
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
+
     case $cur in
         appc)
             COMPREPLY=( $(compgen -W "logon logoff view resource label enable disable restart reg unreg run get put config passwd lock log" -- $cur) )
-            return 0
+            return
             ;;
         -n|--name)
             local APPS=`appc view | awk '{print $2}'`
             COMPREPLY=( $(compgen -W "$APPS" -- $cur) )
-            return 0
+            return
             ;;
         -h|-v)
-            return 0
+            return
             ;;
     esac
-    return 0
 } &&
-complete -F _appc_modeule appc
+complete -F _appc appc
 
 # ex: ts=4 sw=4 et filetype=sh
