caafbdc3e73a4759aa02d24731af7c64c50f507f
laoshanxi <178029200@qq.com>
2020-05-13 08:24:25 +0800

    remvoe topology for offline node

--- src/daemon/ConsulConnection.cpp
+++ src/daemon/ConsulConnection.cpp
@@ -426,8 +426,9 @@ void ConsulConnection::offlineNode()
 	const static char fname[] = "ConsulConnection::offlineNode() ";
 	LOG_DBG << fname;
 
-	writeTopology(MY_HOST_NAME, nullptr);
 	auto path = std::string(CONSUL_BASE_PATH).append("cluster/nodes/").append(MY_HOST_NAME);
+	requestHttp(web::http::methods::DEL, path, {}, {}, nullptr);
+	path = std::string(CONSUL_BASE_PATH).append("topology/").append(MY_HOST_NAME);
 	requestHttp(web::http::methods::DEL, path, {}, {}, nullptr);
 
 	auto currentAllApps = Configuration::instance()->getApps();
