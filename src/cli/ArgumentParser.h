e39232dc2c1c5b50d8b92f11f54ef2d3acc3b9f0
laoshanxi <178029200@qq.com>
2020-09-13 04:06:15 +0800

    format

--- src/cli/ArgumentParser.h
+++ src/cli/ArgumentParser.h
@@ -6,10 +6,10 @@
 #include <cpprest/http_client.h>
 #include <boost/program_options.hpp>
 
-using namespace web;                        // Common features like URIs.
-using namespace web::http;                  // Common HTTP functionality
-using namespace web::http::client;          // HTTP client features
-using namespace concurrency::streams;       // Asynchronous streams
+using namespace web;				  // Common features like URIs.
+using namespace web::http;			  // Common HTTP functionality
+using namespace web::http::client;	  // HTTP client features
+using namespace concurrency::streams; // Asynchronous streams
 
 namespace po = boost::program_options;
 
@@ -19,8 +19,8 @@ namespace po = boost::program_options;
 class ArgumentParser
 {
 public:
-	ArgumentParser(int argc, const char* argv[], int listenPort, bool sslEnabled);
-	virtual ~ArgumentParser();
+	explicit ArgumentParser(int argc, const char *argv[], int listenPort, bool sslEnabled);
+	virtual ~ArgumentParser() noexcept;
 
 	void parse();
 
@@ -44,24 +44,25 @@ private:
 	void processLockUser();
 	void processEncryptUserPwd();
 
-	bool confirmInput(const char* msg);
-	http_response requestHttp(const method& mtd, const std::string& path);
-	http_response requestHttp(const method& mtd, const std::string& path, web::json::value& body);
-	http_response requestHttp(const method& mtd, const std::string& path, std::map<std::string, std::string>& query, web::json::value* body = nullptr, std::map<std::string, std::string>* header = nullptr);
-	http_request createRequest(const method& mtd, const std::string& path, std::map<std::string, std::string>& query, std::map<std::string, std::string>* header);
+	http_response requestHttp(const method &mtd, const std::string &path);
+	http_response requestHttp(const method &mtd, const std::string &path, web::json::value &body);
+	http_response requestHttp(const method &mtd, const std::string &path, std::map<std::string, std::string> &query, web::json::value *body = nullptr, std::map<std::string, std::string> *header = nullptr);
+	http_request createRequest(const method &mtd, const std::string &path, std::map<std::string, std::string> &query, std::map<std::string, std::string> *header);
 
 	std::string getAuthenToken();
 	std::string readAuthenToken();
-	std::string requestToken(const std::string& user, const std::string& passwd);
+	std::string requestToken(const std::string &user, const std::string &passwd);
 
 private:
-	bool isAppExist(const std::string& appName);
+	bool isAppExist(const std::string &appName);
 	std::map<std::string, bool> getAppList();
 	void printApps(web::json::value json, bool reduce);
-	void shiftCommandLineArgs(po::options_description& desc);
+	void shiftCommandLineArgs(po::options_description &desc);
 	std::string reduceStr(std::string source, int limit);
 	void setStdinEcho(bool enable = true);
-	size_t inputSecurePasswd(char** pw, size_t sz, int mask, FILE* fp);
+	bool confirmInput(const char *msg);
+	size_t inputSecurePasswd(char **pw, size_t sz, int mask, FILE *fp);
+
 private:
 	po::variables_map m_commandLineVariables;
 	std::vector<po::option> m_pasrsedOptions;
@@ -71,5 +72,4 @@ private:
 	std::string m_hostname;
 	std::string m_username;
 	std::string m_userpwd;
-
 };
