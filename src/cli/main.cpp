ec1a770c288fc8739692a78e6d7c54175cf6b07c
laoshanxi <178029200@qq.com>
2021-01-11 20:03:46 +0800

    remove getListenPort()

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,18 +1,8 @@
 
-#include <iomanip>
-#include <string>
-
-#include <ace/OS.h>
-#include <boost/program_options.hpp>
-#include <cpprest/http_client.h>
-#include <cpprest/json.h>
 #include <pplx/threadpool.h>
 
 #include "../common/Utility.h"
 #include "ArgumentParser.h"
-
-namespace po = boost::program_options;
-void getListenPort(int &port, bool &sslEnabled);
 
 /// <summary>
 /// Command line entrypoint
@@ -22,11 +12,8 @@ int main(int argc, const char *argv[])
 	PRINT_VERSION();
 	try
 	{
-		int port = DEFAULT_REST_LISTEN_PORT;
-		bool ssl = false;
 		crossplat::threadpool::initialize_with_threads(1);
-		getListenPort(port, ssl);
-		ArgumentParser parser(argc, argv, port, ssl);
+		ArgumentParser parser(argc, argv, DEFAULT_REST_LISTEN_PORT, true);
 		parser.parse();
 	}
 	catch (const std::exception &e)
@@ -35,28 +22,4 @@ int main(int argc, const char *argv[])
 		return -1;
 	}
 	return 0;
-}
-
-void getListenPort(int &port, bool &sslEnabled)
-{
-	// Get listen port
-	web::json::value jsonValue;
-	std::string jsonPath = Utility::getSelfDir() + ACE_DIRECTORY_SEPARATOR_STR + APPMESH_CONFIG_JSON_FILE;
-	auto file = Utility::readFileCpp(jsonPath);
-	if (file.length() > 0)
-	{
-		jsonValue = web::json::value::parse(GET_STRING_T(file));
-		if (HAS_JSON_FIELD(jsonValue, JSON_KEY_REST) &&
-			HAS_JSON_FIELD(jsonValue.at(JSON_KEY_REST), JSON_KEY_RestListenPort))
-		{
-			auto rest = jsonValue.at(JSON_KEY_REST);
-			port = GET_JSON_INT_VALUE(rest, JSON_KEY_RestListenPort);
-			// SSL
-			if (HAS_JSON_FIELD(rest, JSON_KEY_SSL) &&
-				HAS_JSON_FIELD(rest.at(JSON_KEY_SSL), JSON_KEY_SSLEnabled))
-			{
-				sslEnabled = GET_JSON_BOOL_VALUE(rest.at(JSON_KEY_SSL), JSON_KEY_SSLEnabled);
-			}
-		}
-	}
 }
